18ddbe07d73cb344d9cdefc4553edb2d87ee2f1d
Chamalo <yann@bubusissi.ch>
2015-12-11 00:18:20 +0100

    Suppress existing js files references

---
+++ solution/app/Swimmy.Web/Gruntfile.js
@@ -0,0 +1,57 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Copy web assets from bower_components to more convenient directories.
+        copy: {
+            main: {
+                files: [
+                    // Vendor scripts.
+                    {
+                        expand: true,
+                        cwd: 'bower_components/bootstrap/dist/js/',
+                        src: ['**/*.js'],
+                        dest: 'scripts/bootstrap/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/jquery/dist/',
+                        src: ['**/*.js', '**/*.map'],
+                        dest: 'scripts/jquery/'
+                    },
+
+                    // Fonts.
+                    {
+                        expand: true,
+                        filter: 'isFile',
+                        flatten: true,
+                        cwd: 'bower_components/',
+                        src: ['/bootstrap/dist/fonts/**'],
+                        dest: 'fonts/'
+                    },
+
+                    // Stylesheets
+                    {
+                        expand: true,
+                        cwd: 'bower_components/bootstrap/dist/css/',
+                        src: ['**/*.css', '**/*.map'],
+                        dest: 'Content/css/'
+                    }
+                ]
+            }
+        },
+
+        // Watch these files and notify of changes.
+        watch: {
+            grunt: { files: ['Gruntfile.js'] }
+        }
+    });
+
+    // Load externally defined tasks. 
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Establish tasks we can run from the terminal.
+    grunt.registerTask('build', ['copy']);
+    grunt.registerTask('default', ['build', 'watch']);
+}
