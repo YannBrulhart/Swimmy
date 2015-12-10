b9900171d5f025d5c15df9ed8653b0ada3d54dce
Chamalo <yann@bubusissi.ch>
2015-12-11 00:19:58 +0100

    Add grunt.js completed

--- solution/app/Swimmy.Web/Gruntfile.js
+++ solution/app/Swimmy.Web/Gruntfile.js
@@ -2,56 +2,137 @@
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        // Copy web assets from bower_components to more convenient directories.
         copy: {
             main: {
                 files: [
-                    // Vendor scripts.
+                    {
+                        expand: true,
+                        cwd: 'bower_components/angucomplete-alt/dist/',
+                        src: ['**/*.js', '**/*.map'],
+                        dest: 'scripts/Vendors/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/angular/dist/',
+                        src: ['**/*.js', '**/*.map'],
+                        dest: 'scripts/Vendors/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/angular/dist/',
+                        src: ['**/*.css'],
+                        dest: 'Content/css/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/angular-base64/',
+                        src: ['**/*.js', '**/*.map'],
+                        dest: 'scripts/Vendors/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/angular-bootstrap/',
+                        src: ['**/*.js', '**/*.map'],
+                        dest: 'scripts/Vendors/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/angular-bootstrap/',
+                        src: ['**/*.css'],
+                        dest: 'Content/css/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/angular-file-upload/dist',
+                        src: ['**/*.js', '**/*.map'],
+                        dest: 'scripts/Vendors/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/angular-loading-bar/build/',
+                        src: ['**/*.js', '**/*.map'],
+                        dest: 'scripts/Vendors/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/angular-loading-bar/build/',
+                        src: ['**/*.css'],
+                        dest: 'Content/css/'
+                    },
                     {
                         expand: true,
                         cwd: 'bower_components/bootstrap/dist/js/',
                         src: ['**/*.js'],
-                        dest: 'scripts/bootstrap/'
+                        dest: 'scripts/Vendors/'
                     },
                     {
                         expand: true,
-                        cwd: 'bower_components/jquery/dist/',
-                        src: ['**/*.js', '**/*.map'],
-                        dest: 'scripts/jquery/'
+                        cwd: 'bower_components/bootstrap/dist/fonts/',
+                        src: ['**/*.*'],
+                        dest: 'Content/fonts/'
                     },
-
-                    // Fonts.
-                    {
-                        expand: true,
-                        filter: 'isFile',
-                        flatten: true,
-                        cwd: 'bower_components/',
-                        src: ['/bootstrap/dist/fonts/**'],
-                        dest: 'fonts/'
-                    },
-
-                    // Stylesheets
                     {
                         expand: true,
                         cwd: 'bower_components/bootstrap/dist/css/',
                         src: ['**/*.css', '**/*.map'],
                         dest: 'Content/css/'
-                    }
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/jquery/dist/',
+                        src: ['**/*.js', '**/*.map'],
+                        dest: 'scripts/Vendors/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/raty/lib/',
+                        src: ['**/*.css'],
+                        dest: 'Content/css/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/raty/lib/',
+                        src: ['**/*.js'],
+                        dest: 'scripts/Vendors/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/raty/lib/fonts',
+                        src: ['**/*.*'],
+                        dest: 'Content/fonts/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/raty/lib/images',
+                        src: ['**/*.png'],
+                        dest: 'Content/images/raty'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/tg-angular-validator/dist/',
+                        src: ['**/*.js', '**/*.map'],
+                        dest: 'scripts/Vendors/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/toastr/',
+                        src: ['**/*.js', '**/*.map'],
+                        dest: 'scripts/Vendors/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/toastr/',
+                        src: ['**/*.css', '**/*.map'],
+                        dest: 'Content/css/'
+                    },
                 ]
             }
-        },
-
-        // Watch these files and notify of changes.
-        watch: {
-            grunt: { files: ['Gruntfile.js'] }
-        }
+        }        
     });
 
     // Load externally defined tasks. 
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Establish tasks we can run from the terminal.
-    grunt.registerTask('build', ['copy']);
-    grunt.registerTask('default', ['build', 'watch']);
+    grunt.registerTask('default', ['copy']);
 }
